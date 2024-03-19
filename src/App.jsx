b547bf962bcd220992643b99b36c7d15cd04c352
Munib Ahmed <munibahmed016@gmail.com>
2024-03-19 09:29:56 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlight from "./components/Highlights";
+
+const  App = () => {
+  return (
+   <main className="bg-black">
+    <Navbar/>
+    <Hero/>
+    <Highlight/> 
+
+   </main>
+  )
+}
+
+export default App
