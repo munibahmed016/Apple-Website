c61c3d63285aed2a70a8ac28a32b33818b13cb3e
Munib Ahmed <munibahmed016@gmail.com>
2024-03-22 23:39:39 +0500

    final update

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -198,11 +198,11 @@ const VideoCarousel = () => {
       </div>
 
       <div className="relative flex-center mt-10">
-        <div className="flex-center py-5 px-7 bg-gray-700 backdrop-blur rounded-full">
+        <div className="flex-center py-5 px-7 bg-gray-300 backdrop-blur rounded-full">
           {videoRef.current.map((_, i) => (
             <span
               key={i}
-              className="mx-2 w-3 h-3 bg-gray-400 rounded-full relative cursor-pointer"
+              className="mx-2 w-3 h-3 bg-gray-200 rounded-full relative cursor-pointer"
               ref={(el) => (videoDivRef.current[i] = el)}
             >
               <span
